93a58e5de55adab297b37b2e72f4dfb01f140acf
Durgeswari-2004 <108514700+Durgeswari-2004@users.noreply.github.com>
2023-03-31 12:13:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Day from './components/day';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <Day/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
